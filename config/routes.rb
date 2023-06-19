41e22d005d7b198e57c21d101030b9ea70e36c90
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-06-19 18:12:43 +0300

    lab completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'students/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'cheeses', to: 'cheeses#index'
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
